bf8cc7a26be8659a7757f28ba642d161e9d9102c
Tomás Hernández <hernandeztomas584@gmail.com>
2022-09-04 19:56:29 -0300

    Lazy Load & Suspense ADDED

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,45 +1,51 @@
+import { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   NavLink
 } from 'react-router-dom';
-import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages/index';
 
 
 import logo from '../logo.svg';
+import { routes } from './routes';
 
 export const Navigation = () => {
   return (
-    <Router>
-      <div className="main-layout">
-        <nav>
-            <img src={ logo } alt="React Logo" />
-          <ul>
-            <li>
-              <NavLink to="/lazy1" activeClassName="nav-active" exact>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy2" activeClassName="nav-active" exact>About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/lazy3" activeClassName="nav-active" exact>Users</NavLink>
-            </li>
-          </ul>
-        </nav>
+    <Suspense fallback={null}>
+      <Router>
+        <div className="main-layout">
+          <nav>
+              <img src={ logo } alt="React Logo" />
+            <ul>
 
-        <Switch>
-          <Route path="/lazy1">
-            <LazyPage1/>
-          </Route>
-          <Route path="/lazy2">
-            <LazyPage2/>
-          </Route>
-          <Route path="/lazy3">
-            <LazyPage3/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+              {
+                routes.map(({name, to})=>{
+                  return ( 
+                  <li key={name}>
+                    <NavLink to={to} activeClassName="nav-active" exact>{name}</NavLink>
+                  </li>)
+                })
+              }
+            
+            </ul>
+          </nav>
+
+          <Switch>
+      
+            {
+                routes.map(({name, path, Component})=>{
+                  return ( 
+                    <Route key={name} path={path}>
+                        <Component/>
+                    </Route>
+                  )
+                })
+            }
+              
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
   );
 }
