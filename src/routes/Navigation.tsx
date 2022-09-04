3d36c1696b9a25a6eb65d13c85814693c0fa6b85
Tomás Hernández <hernandeztomas584@gmail.com>
2022-09-04 19:18:47 -0300

    Componentes Lazy agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -4,6 +4,8 @@ import {
   Route,
   NavLink
 } from 'react-router-dom';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages/index';
+
 
 import logo from '../logo.svg';
 
@@ -15,28 +17,26 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
+              <NavLink to="/lazy1" activeClassName="nav-active" exact>Home</NavLink>
             </li>
             <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              <NavLink to="/lazy2" activeClassName="nav-active" exact>About</NavLink>
             </li>
             <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              <NavLink to="/lazy3" activeClassName="nav-active" exact>Users</NavLink>
             </li>
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <h1>About</h1>
+          <Route path="/lazy1">
+            <LazyPage1/>
           </Route>
-          <Route path="/users">
-            <h1>Users</h1>
+          <Route path="/lazy2">
+            <LazyPage2/>
           </Route>
-          <Route path="/">
-            <h1>Home</h1>
+          <Route path="/lazy3">
+            <LazyPage3/>
           </Route>
         </Switch>
       </div>
