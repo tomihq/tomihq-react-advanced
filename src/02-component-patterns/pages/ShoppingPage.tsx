cb1c888968bd797355cf60f0ad5bde02d26a5c0e
Tomás Hernández <hernandeztomas584@gmail.com>
2023-03-04 22:21:29 -0300

    feat: add useProduct custom hook

---
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ProductCard } from '../components/ProductCard';
+
+export const ShoppingPage = () => {
+  return (
+    <div><h1>Shopping Store</h1> <hr /> <ProductCard/> </div>
+  )
+}
